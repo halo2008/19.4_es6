85c5ac7b15658cccdfa54895114bbc6b425ebbab
Konrad Sedkowski <ksedkowski@gmail.com>
2019-01-15 17:31:37 +0100

    fixed bug

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ const b = 'world';
 const sayHello = () => console.log(`${a} ${b}`);
 
 //zadanie 2
-multiply(2)
 const multiply = (x,y=1) => x*y;
+console.log(multiply(2));
 
 //zadanie 3
-average(1, 3, 6, 6)
 var sum = (num1, num2) => {
  var b = num1 + num2
    return b/b;
@@ -20,12 +19,12 @@ function average(...numbers) {
   const sum = numbers.reduce(callback, 0);
   return sum/numLen;
 }
-
+console.log(average(1, 3, 6, 6));
 //zadanie 4
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 console.log(average(...grades));
 
 //zadanie 5
 const names = [1, 4, 'Iwona', false, 'Nowak'];
-const [,,,fristName,,lastName] = names;
+const [,,fristName,,lastName] = names;
 console.log(fristName,lastName);
